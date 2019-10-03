555e4ee903758ca053ada14c211435287dd35960
naphtha <naphthanewman@gmail.com>
2019-10-03 23:14:55 +0000

    portfolio hero section and layout ready

---
+++ js/script.js
@@ -0,0 +1,128 @@
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-1",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-2",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-3",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+  .add({
+    targets: ".ml4 .letters-4",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-4",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+
+  .add({
+    targets: ".ml4 .letters-5",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-5",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+
+  .add({
+    targets: ".ml4 .letters-6",
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  })
+  .add({
+    targets: ".ml4 .letters-6",
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  })
+
+  .add({
+    targets: ".ml4",
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml3 .letter",
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i + 1)
+  })
+  .add({
+    targets: ".ml3",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
